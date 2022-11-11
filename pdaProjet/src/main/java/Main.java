9a33296de7109a9fcb0dc5bdbfcd7236ffcff269
tahaDourhmi <freelincertaha@gmail.com>
2022-11-11 16:27:59 +0100

    pda projet

--- pdaProjet/src/main/java/Main.java
+++ pdaProjet/src/main/java/Main.java
@@ -1,15 +1,8 @@
-import com.java.Dao.UserDao;
-import com.java.Entitys.User;
+import com.pdaProjet.Dao.UserDao;
+
 public class Main {
-
-	public static void main(String[] args) {
-		UserDao userdao = new UserDao();
-		User user = new User("taha","taha@gmail.com","taha2000",234234);
-		userdao.insertEntity(user);
-		System.out.print("affichage de user ");
-		userdao.findEntity(1);
-		
-		
-	}
-
+    public static void main(String[] args) {
+        UserDao  dao = new UserDao();
+        dao.insertEntity();
+    }
 }
